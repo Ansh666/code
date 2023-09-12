2ca215896ca25507e1ec5185b6b59caec215c7da
wetrainsmart <wetrainsmart@gmail.com>
2023-09-12 18:59:08 +0530

    SETCODECHAVO

---
+++ src/component/atom/Card.js
@@ -0,0 +1,8 @@
+import React from "react";
+import cardblueicon from "../../assets/cardblueicon.svg";
+
+function Card({ className, children }) {
+  return <div className={className}>{children}</div>;
+}
+
+export default Card;
---
+++ src/component/atom/ChartCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ChartCard({ children }) {
+  return <div className="chartCard">{children}</div>;
+}
+
+export default ChartCard;
